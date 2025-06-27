6d13b3cbd407001feb1f613e4f732013211c081a
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-06-27 22:21:02 +0200

    added more jni function bodies

--- jni/src/types.rs
+++ jni/src/types.rs
@@ -199,6 +199,247 @@ impl JNINativeInterface_ {
     pub fn NewObjectA(&mut self, methodID: jmethodID, args: *const jvalue) -> jobject{
         unimplemented!()
     }
+
+    pub fn GetObjectClass(obj: jobject) -> jclass{
+        unimplemented!()
+    }
+    pub fn IsInstanceOf(obj: jobject, clazz: jclass){
+        unimplemented!()
+    }
+
+    pub fn GetMethodID(clazz: jclass, name: *const c_char, sig: *const c_char) -> jmethodID{
+        unimplemented!()
+    }
+
+    pub fn CallObjectMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jobject{
+        unimplemented!()
+    }
+    pub fn CallBooleanMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jboolean{
+        unimplemented!()
+    }
+    pub fn CallByteMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jbyte{
+        unimplemented!()
+    }
+    pub fn CallCharMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jchar{
+        unimplemented!()
+    }
+    pub fn CallShortMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jshort{
+        unimplemented!()
+    }
+    pub fn CallIntMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jint{
+        unimplemented!()
+    }
+    pub fn CallLongMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jlong{
+        unimplemented!()
+    }
+    pub fn CallFloatMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jfloat{
+        unimplemented!()
+    }
+    pub fn CallDoubleMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue) -> jdouble{
+        unimplemented!()
+    }
+    pub fn CallVoidMethodA(obj: jobject, methodID: jmethodID, args: *const jvalue){
+        unimplemented!()
+    }
+
+    pub fn CallNonvirtualObjectMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jobject{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualBooleanMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jboolean{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualByteMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jbyte{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualCharMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jchar{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualShortMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jshort{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualIntMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jint{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualLongMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jlong{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualFloatMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jfloat{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualDoubleMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jdouble{
+        unimplemented!()
+    }
+    pub fn CallNonvirtualVoidMethodA(obj: jobject, clazz: jclass, methodID: jmethodID, args: *const jvalue){
+        unimplemented!()
+    }
+
+    pub fn GetFieldID(clazz: jclass, name: *const c_char, sig: *const c_char) -> jfieldID{
+        unimplemented!()
+    }
+
+    pub fn GetObjectField(obj: jobject, fieldID: jfieldID) -> jobject{
+        unimplemented!()
+    }
+    pub fn GetBooleanField(obj: jobject, fieldID: jfieldID) -> jboolean{
+        unimplemented!()
+    }
+    pub fn GetByteField(obj: jobject, fieldID: jfieldID) -> jbyte{
+        unimplemented!()
+    }
+    pub fn GetCharField(obj: jobject, fieldID: jfieldID) -> jchar{
+        unimplemented!()
+    }
+    pub fn GetShortField(obj: jobject, fieldID: jfieldID) -> jshort{
+        unimplemented!()
+    }
+    pub fn GetIntField(obj: jobject, fieldID: jfieldID) -> jint{
+        unimplemented!()
+    }
+    pub fn GetLongField(obj: jobject, fieldID: jfieldID) -> jlong{
+        unimplemented!()
+    }
+    pub fn GetFloatField(obj: jobject, fieldID: jfieldID) -> jfloat{
+        unimplemented!()
+    }
+    pub fn GetDoubleField(obj: jobject, fieldID: jfieldID) -> jdouble{
+        unimplemented!()
+    }
+
+    pub fn SetObjectField(obj: jobject, fieldID: jfieldID, val: jobject){
+        unimplemented!()
+    }
+    pub fn SetBooleanField(obj: jobject, fieldID: jfieldID, val: jboolean){
+        unimplemented!()
+    }
+    pub fn SetByteField(obj: jobject, fieldID: jfieldID, val: jbyte){
+        unimplemented!()
+    }
+    pub fn SetCharField(obj: jobject, fieldID: jfieldID, val: jchar){
+        unimplemented!()
+    }
+    pub fn SetShortField(obj: jobject, fieldID: jfieldID, val: jshort){
+        unimplemented!()
+    }
+    pub fn SetIntField(obj: jobject, fieldID: jfieldID, val: jint){
+        unimplemented!()
+    }
+    pub fn SetLongField(obj: jobject, fieldID: jfieldID, val: jlong){
+        unimplemented!()
+    }
+    pub fn SetFloatField(obj: jobject, fieldID: jfieldID, val: jfloat){
+        unimplemented!()
+    }
+    pub fn SetDoubleField(obj: jobject, fieldID: jfieldID, val: jdouble){
+        unimplemented!()
+    }
+
+    pub fn GetStaticMethodID(clazz: jclass, name: *const c_char, sig: *const c_char) -> jmethodID{
+        unimplemented!()
+    }
+
+    pub fn CallStaticObjectMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jobject{
+        unimplemented!()
+    }
+    pub fn CallStaticBooleanMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jboolean{
+        unimplemented!()
+    }
+    pub fn CallStaticByteMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jbyte{
+        unimplemented!()
+    }
+    pub fn CallStaticCharMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jchar{
+        unimplemented!()
+    }
+    pub fn CallStaticShortMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jshort{
+        unimplemented!()
+    }
+    pub fn CallStaticIntMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jint{
+        unimplemented!()
+    }
+    pub fn CallStaticLongMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jlong{
+        unimplemented!()
+    }
+    pub fn CallStaticFloatMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jfloat{
+        unimplemented!()
+    }
+    pub fn CallStaticDoubleMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue) -> jdouble{
+        unimplemented!()
+    }
+    pub fn CallStaticVoidMethodA(clazz: jclass, methodID: jmethodID, args: *const jvalue){
+        unimplemented!()
+    }
+
+    pub fn GetStaticFieldID(clazz: jclass, name: *const c_char, sig: *const c_char) -> jfieldID{
+        unimplemented!()
+    }
+
+    pub fn GetStaticObjectField(clazz: jclass, fieldID: jfieldID) -> jobject{
+        unimplemented!()
+    }
+    pub fn GetStaticBooleanField(clazz: jclass, fieldID: jfieldID) -> jboolean{
+        unimplemented!()
+    }
+    pub fn GetStaticByteField(clazz: jclass, fieldID: jfieldID) -> jbyte{
+        unimplemented!()
+    }
+    pub fn GetStaticCharField(clazz: jclass, fieldID: jfieldID) -> jchar{
+        unimplemented!()
+    }
+    pub fn GetStaticShortField(clazz: jclass, fieldID: jfieldID) -> jshort{
+        unimplemented!()
+    }
+    pub fn GetStaticIntField(clazz: jclass, fieldID: jfieldID) -> jint{
+        unimplemented!()
+    }
+    pub fn GetStaticLongField(clazz: jclass, fieldID: jfieldID) -> jlong{
+        unimplemented!()
+    }
+    pub fn GetStaticFloatField(clazz: jclass, fieldID: jfieldID) -> jfloat{
+        unimplemented!()
+    }
+    pub fn GetStaticDoubleField(clazz: jclass, fieldID: jfieldID) -> jdouble{
+        unimplemented!()
+    }
+
+    pub fn SetStaticObjectField(clazz: jclass, fieldID: jfieldID, val: jobject){
+        unimplemented!()
+    }
+    pub fn SetStaticBooleanField(clazz: jclass, fieldID: jfieldID, val: jboolean){
+        unimplemented!()
+    }
+    pub fn SetStaticByteField(clazz: jclass, fieldID: jfieldID, val: jbyte){
+        unimplemented!()
+    }
+    pub fn SetStaticCharField(clazz: jclass, fieldID: jfieldID, val: jchar){
+        unimplemented!()
+    }
+    pub fn SetStaticShortField(clazz: jclass, fieldID: jfieldID, val: jshort){
+        unimplemented!()
+    }
+    pub fn SetStaticIntField(clazz: jclass, fieldID: jfieldID, val: jint){
+        unimplemented!()
+    }
+    pub fn SetStaticLongField(clazz: jclass, fieldID: jfieldID, val: jlong){
+        unimplemented!()
+    }
+    pub fn SetStaticFloatField(clazz: jclass, fieldID: jfieldID, val: jfloat){
+        unimplemented!()
+    }
+    pub fn SetStaticDoubleField(clazz: jclass, fieldID: jfieldID, val: jdouble){
+        unimplemented!()
+    }
+
+    pub fn NewString(unicode: *const c_char, len: jsize) -> jstring{
+        unimplemented!()
+    }
+    pub fn GetStringLength(str: jstring) -> jsize{
+        unimplemented!()
+    }
+    pub fn GetStringChars(str: jstring, isCopy: *mut jboolean) -> *const jchar{
+        unimplemented!()
+    }
+    pub fn ReleaseStringChars(str: jstring, chars: *const jchar){
+        unimplemented!()
+    }
 }
 
 #[repr(C)]
