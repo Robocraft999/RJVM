af783006384780a01e6166dcf1390f5aa02e8b10
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-11-16 03:28:41 +0100

    capsulation of lib.rs code to application struct should help to implement natives with JavaVM access

---
+++ src/application.rs
@@ -0,0 +1,84 @@
+use log::error;
+use crate::vm::jni::types::JavaVM;
+use crate::vm::{VmError, VM};
+use crate::vm::result::VMResultType;
+use crate::vm::value::Value;
+
+pub struct Application<'a>{
+    java_vm: JavaVM<'a>,
+    vm: VM<'a>,
+}
+
+impl <'a> Application<'a>{
+    pub fn new(java_vm: JavaVM<'a>, vm: VM<'a>) -> Self{
+        Self { java_vm, vm }
+    }
+
+    fn init_vm(&self) -> Result<(), VmError>{
+        if let VMResultType::NeedsClassInit(classes, _) = self.vm.get_or_resolve_class("sun/misc/VM")?{
+            for frame in classes{
+                self.vm.invoke_current_frame()?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn init_system(&self) -> Result<(), VmError>{
+        if let VMResultType::NeedsClassInit(classes, _) = self.vm.get_or_resolve_class("java/lang/System")?{
+            for frame in classes{
+                self.vm.invoke_current_frame()?;
+            }
+        }
+        let init = self.vm.try_resolve_class_method("java/lang/System", "initializeSystemClass", "()V")?;
+        self.vm.invoke_new_frame(init, None, vec![])?;
+        Ok(())
+    }
+
+    pub fn run_and_catch_method(&self, class_name: &str, method_name: &str, method_descriptor: &str, args: Vec<Value<'a>>){
+        if let VMResultType::NeedsClassInit(classes, _) = self.vm.get_or_resolve_class(class_name).unwrap().clone(){
+            for frame in classes{
+                self.vm.invoke_current_frame().unwrap();
+            }
+        }
+        let main_method = self.vm.try_resolve_class_method(class_name, method_name, method_descriptor).unwrap();
+        let result = self.vm.invoke_new_frame(main_method, None, args);
+        match result {
+            Ok(res) => {
+                println!("result: {res:?}");
+            }
+            Err(error) => {
+                error!("Error: {}", error);
+                println!("Frames:");
+                self.vm.call_stack.print_call_stack();
+            }
+        }
+    }
+    
+    pub fn startup(&self){
+        match self.init_vm() {
+            Ok(_) => {}
+            Err(error) => {
+                error!("Init VM: {}", error);
+                panic!();
+            }
+        };
+        match self.init_system(){
+            Ok(_) => {}
+            Err(error) => {
+                println!("'{}'", error);
+                error!("Init System: {}", error);
+                #[cfg(feature = "debug")]
+                {
+                    self.vm.debug_helper.exception_helper.print();
+                }
+                panic!();
+            }
+        }
+        #[cfg(feature = "debug")]
+        {
+            self.vm.debug_helper.exception_helper.print();
+        }
+        println!("Init complete. Starting Main Program");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,17 @@ use std::io::Read;
 use std::str::FromStr;
 use cesu8::from_java_cesu8;
 use log::{error, info, warn, LevelFilter};
-use access_flags::{parse_class_flags, parse_field_flags, parse_method_flags};
-use attribute::{Attribute, Code};
 use vm::class_path::ClassPath;
 
-use crate::access_flags::ClassFlags;
-use crate::attribute::{ConstantValue, LineNumber, LineNumberTable, LineNumberTableEntry, ProgramCounter};
-use crate::bytes::{parse_u1, parse_u2, parse_u4, parse_u8};
-use crate::class_file_version::ClassFileVersion;
-use crate::constants::*;
-use crate::error::ClassParseError;
+use crate::application::Application;
+use crate::attribute::{ProgramCounter};
 use crate::field_info::{FieldInfo, FieldType};
-use crate::method_info::{MethodDescriptor, MethodInfo};
 use crate::vm::jni::types::{JNIEnv, JavaVM};
 use crate::vm::jni::{self};
-use crate::vm::{VM, VmError};
-use crate::vm::class::{ClassAndMethod, ClassRef};
-use crate::vm::result::VMResultType;
+use crate::vm::{VmError, VM};
 use crate::vm::value::Value;
 
+mod application;
 mod constants;
 mod bytes;
 mod access_flags;
@@ -79,61 +71,12 @@ macro_rules! get_or_init_special {
     };
 }
 
-fn init_vm(vm: &VM) -> Result<(), VmError>{
-    if let VMResultType::NeedsClassInit(classes, _) = vm.get_or_resolve_class("sun/misc/VM")?{
-        for frame in classes{
-            vm.invoke_current_frame()?;
-        }
-    }
-
-    Ok(())
-}
-
-fn init_system(vm: &VM) -> Result<(), VmError>{
-    if let VMResultType::NeedsClassInit(classes, _) = vm.get_or_resolve_class("java/lang/System")?{
-        for frame in classes{
-            vm.invoke_current_frame()?;
-        }
-    }
-    let init = vm.try_resolve_class_method("java/lang/System", "initializeSystemClass", "()V")?;
-    vm.invoke_new_frame(init, None, vec![])?;
-    Ok(())
-}
-
-fn run_and_catch_method<'a>(vm: &'a VM<'a>, class_name: &str, method_name: &str, method_descriptor: &str, args: Vec<Value<'a>>){
-    if let VMResultType::NeedsClassInit(classes, _) = vm.get_or_resolve_class(class_name).unwrap().clone(){
-        for frame in classes{
-            vm.invoke_current_frame().unwrap();
-        }
-    }
-    let main_method = vm.try_resolve_class_method(class_name, method_name, method_descriptor).unwrap();
-    let result = vm.invoke_new_frame(main_method, None, args);
-    match result {
-        Ok(res) => {
-            println!("result: {res:?}");
-            for (id, static_object) in vm.static_class_objects.borrow().iter(){
-                //println!("[{}] {:?}", vm.find_class_by_id(id.clone()).unwrap().name, static_object);
-            }
-        }
-        Err(error) => {
-            error!("Error: {}", error);
-            //vm.call_stack.print_call_stack();
-            println!("Frames:");
-            vm.call_stack.print_call_stack();
-            /*for (index, call_frame_info) in vm.call_stack.frames.iter().enumerate(){
-                //error!("[{}]: {:?}, stack={}, locals={}", index, call_frame.pc, call_frame.stack, call_frame.locals);
-                println!("[{}]: {:?}", index, call_frame_info);
-            }*/
-        }
-    }
-}
-
 pub fn run() {
     let mut class_path = ClassPath::default();
     class_path.push("../resources/rt.jar;../resources/LogicSim.jar;../resources/lib/unix;../resources/lib").expect("TODO: panic message");
 
     println!("Booting up VM");
-    let mut vm = VM::new(class_path);
+    let vm = VM::new(class_path);
 
     simple_logger::SimpleLogger::new().with_level(LevelFilter::Error).without_timestamps().init().unwrap();
     let env = JNIEnv{
@@ -159,39 +102,8 @@ pub fn run() {
         let res: i32 = cif.call(libffi::low::CodePtr::from_ptr(func_ptr), &[Arg::new(&vm_ptr), Arg::new(&reserved)]);
         
     }
-    
-    //simple_logger::SimpleLogger::new().with_level(LevelFilter::Trace).without_timestamps().init().unwrap();
-    //run_and_catch_method(&mut vm, "Test", "main", "([Ljava/lang/String;)V");
-    //return
-
-    match init_vm(&vm) {
-        Ok(_) => {}
-        Err(error) => {
-            error!("Init VM: {}", error);
-            panic!();
-        }
-    };
-    //simple_logger::SimpleLogger::new().with_level(LevelFilter::Trace).without_timestamps().init().unwrap();
-    //vm.get_or_resolve_class("java/lang/CharacterData").expect("msg");
-    match init_system(&vm){
-        Ok(_) => {}
-        Err(error) => {
-            println!("'{}'", error);
-            error!("Init System: {}", error);
-            #[cfg(feature = "debug")]
-            {
-                vm.debug_helper.exception_helper.print();
-            }
-            panic!();
-        }
-    }
-
-    #[cfg(feature = "debug")]
-    {
-        vm.debug_helper.exception_helper.print();
-    }
-
-    println!("Init complete. Starting Main Program");
+    let mut app = Application::new(javavm, vm);
+    app.startup();
     todo!("Init complete");
 
     //vm.class_manager.get_or_resolve_class("Empty").expect("TODO: panic message");
@@ -202,7 +114,7 @@ pub fn run() {
     let p_args = vec![Value::Reference(args_array)];
     //run_and_catch_method(&mut vm, "de/klassenserver7b/k7bot/Main", "main", "([Ljava/lang/String;)V", p_args);
     //run_and_catch_method(&mut vm, "Hello", "main", "([Ljava/lang/String;)V", p_args);
-    run_and_catch_method(&mut vm, "logicsim/App", "main", "([Ljava/lang/String;)V", p_args);
+    app.run_and_catch_method("logicsim/App", "main", "([Ljava/lang/String;)V", p_args);
 
     //parse_class_file(&class_path, "java/lang/Exception");
 
